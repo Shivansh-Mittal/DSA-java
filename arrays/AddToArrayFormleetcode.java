8cb836f69712c3a8db08ee182db6f8514bc96c97
Shivansh Mittal <shivanshmittal1444@gmail.com>
2024-11-21 23:14:10 +0530

    Update AddToArrayFormleetcode.java

--- arrays/AddToArrayFormleetcode.java
+++ arrays/AddToArrayFormleetcode.java
@@ -15,36 +15,19 @@ public class AddToArrayFormleetcode {
         System.out.println(res.toString());
     }
 
-    static List<Integer> addToArrayForm(int[] num, int k){
-        List<Integer> arr = new ArrayList<Integer>();
-        // String a = "";
-        int b = 0;
-        for(int i: num){
-            b = b*10 + i;
-        }
-        System.out.println(b);
-        // a = Arrays.toString(num);
-        // b = Integer.valueOf(a);
-        b = b + k;
-        while(b != 0){
-            int digit = b%10;
-            arr.add(digit);
-            b /= 10;
-        }
-        reverse(arr);
-        return arr;
-    }
+    static List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> res = new ArrayList<>();
+        int carry = 0;
 
-    static void reverse(List<Integer> arr){
-        int s = 0;
-        int e = arr.size() - 1;
-
-        while(s<e){
-            int temp = arr.get(s);
-            arr.set(s, arr.get(e));
-            arr.set(e, temp);
-            s++;
-            e--;
+        for (int i = num.length - 1; i >= 0 || k > 0 || carry > 0; i--) {
+            int x = (i >= 0) ? num[i] : 0;
+            int sum = x + k % 10 + carry;
+            res.add(sum % 10);
+            carry = sum / 10;
+            k = k / 10;
         }
+
+        Collections.reverse(res);
+        return res;
     }
 }
